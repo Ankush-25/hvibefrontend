7303dc7088391a4de399ea0541760102460dde12
Ankush-25 <repppol.me@gmail.com>
2025-08-01 22:45:05 +0530

    changes

--- src/assets/Global_Need_files/ImagesPaths.js
+++ src/assets/Global_Need_files/ImagesPaths.js
@@ -1,27 +1,28 @@
 export const Imagepaths = {
-    HiringstoreslogoPath: "/src/assets/Hiring_stores_logo.png",
+    HiringstoreslogoPath: "/Hiring_stores_logo.png",
 }
 
 export const CompaniesLogoPaths={
-    Accenture: "/src/assets/Accenture.png",
-    Amazon: "/src/assets/Amazon.png",
-    Apple: "/src/assets/Apple.png",
-    Google: "/src/assets/Google.png",
-    IBM: "/src/assets/IBM.png",
-    Meta: "/src/assets/Meta.png",
-    Microsoft: "/src/assets/Microsoft.png",
-    Netflix: "/src/assets/Netflix.png",
-    Nvidia: "/src/assets/Nvidia.png",
+    Accenture: "/Accenture.png",
+    Amazon: "/Amazon.png",
+    Apple: "/Apple.png",
+    Google: "/Google.png",
+    IBM: "/IBM.png",
+    Meta: "/Meta.png",
+    Microsoft: "/Microsoft.png",
+    Netflix: "/Netflix.png",
+    Nvidia: "/Nvidia.png",
 }
+
 export const SecondCompaniesLogoPaths = {
-    Salesforce: "/src/assets/salesforce.png",
-    Oracle: "/src/assets/oracle.png",
-    Adobe: "/src/assets/Adobe.png",
-    Intel: "/src/assets/intel.svg",
-    Cisco: "/src/assets/cisco.jpeg",
-    SAP: "/src/assets/sap.png",
-    Dropbox: "/src/assets/dropbox.png",
-    Spotify: "/src/assets/spotify.png",
-    Tcs: "/src/assets/tcs.png",
-  };
+    Salesforce: "/salesforce.png",
+    Oracle: "/oracle.png",
+    Adobe: "/Adobe.png",
+    Intel: "/intel.svg",
+    Cisco: "/cisco.jpeg",
+    SAP: "/sap.png",
+    Dropbox: "/dropbox.png",
+    Spotify: "/spotify.png",
+    Tcs: "/tcs.png",
+};
   
